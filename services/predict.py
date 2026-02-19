78d90f31048ae9b14ae9c1b8267e25df33bd8f92
Elmasry-631 <irahimelmasry631@gmail.com>
2026-02-19 15:26:56 +0200

    ai engine

--- services/predict.py
+++
@@ -1,9 +0,0 @@
-from core.search import search_image
-
-def predict(image_path):
-    label, distance = search_image(image_path)
-
-    if label == "NEW_CLASS":
-        print(f"🚀 New class detected! Distance: {distance}")
-    else:
-        print(f"✅ Belongs to: {label} | Distance: {distance}")
